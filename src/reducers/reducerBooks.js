884f6968c6cc5e5bd3265d4dbc26155e07955bae
Dominic Scanlan <dominicscanlan@hotmail.com>
2016-04-26 17:47:30 +0100

    first

---
+++ src/reducers/reducerBooks.js
@@ -0,0 +1,8 @@
+export default function(){
+	return [
+		{title: 'javascript 1', pages: 101},
+		{title: 'harry potter 2', pages: 12},
+		{title: 'jamesbonder 3', pages: 222},
+		{title: 'john connolly 4', pages: 1029},
+	];
+};
